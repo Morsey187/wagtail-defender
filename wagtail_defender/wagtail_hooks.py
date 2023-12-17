06ab09d05df4828f3e8d081e374bbe2aa1a878b7
Ben Morse <96morse@gmail.com>
2023-12-17 19:52:47 +0000

    Add ModelViewSet for AccessAttempts

--- wagtail_defender/wagtail_hooks.py
+++ wagtail_defender/wagtail_hooks.py
@@ -1,26 +1,30 @@
-from django.urls import include, path
-from django.views.i18n import JavaScriptCatalog
+from defender.models import AccessAttempt
 from wagtail import hooks
+from wagtail.admin.viewsets.base import ViewSetGroup
+from wagtail.admin.viewsets.model import ModelViewSet
 
 
-@hooks.register("register_admin_urls")
-def register_admin_urls():
-    urls = [
-        path(
-            "jsi18n/",
-            JavaScriptCatalog.as_view(packages=["wagtail_defender"]),
-            name="javascript_catalog",
-        ),
-        # Add your other URLs here, and they will appear under `/admin/defender/`
-        # Note: you do not need to check for authentication in views added here, Wagtail does this for you!
+class AccessAttemptViewSet(ModelViewSet):
+    model = AccessAttempt
+    form_fields = []
+    list_display = ["username", "ip_address", "attempt_time", "login_valid"]
+    icon = "key"
+    inspect_view_enabled = True
+    menu_order = 300
+
+
+class AccessAttemptViewSetGroup(ViewSetGroup):
+    items = [
+        AccessAttemptViewSet,
     ]
+    add_to_admin_menu = True
+    menu_order = 8999
+    menu_label = "Defender"
+    menu_icon = "lock"
 
-    return [
-        path(
-            "defender/",
-            include(
-                (urls, "wagtail_defender"),
-                namespace="wagtail_defender",
-            ),
-        )
-    ]
+
+@hooks.register("register_admin_viewset")
+def register_viewset():
+    return AccessAttemptViewSetGroup()
+
+
